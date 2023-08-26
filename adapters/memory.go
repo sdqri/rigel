25913a0753796280f32b62d6437c36755166e63e
sadiq <sadiq>
2023-08-26 13:43:34 +0330

    FIX: Change memory log from lfu to lru

--- adapters/memory.go
+++ adapters/memory.go
@@ -3,14 +3,14 @@ package adapters
 import (
 	"fmt"
 
-	"github.com/Code-Hex/go-generics-cache/policy/lfu"
+	"github.com/Code-Hex/go-generics-cache/policy/lru"
 	log "github.com/sirupsen/logrus"
 )
 
 type MemoryClient struct {
 	LogEntry *log.Entry
 	Prefix   string
-	lfuCache *lfu.Cache[string, string]
+	lfuCache *lru.Cache[string, string]
 }
 
 func NewMemoryClient(logEntry *log.Entry, prefix string, cap int) *MemoryClient {
@@ -18,7 +18,7 @@ func NewMemoryClient(logEntry *log.Entry, prefix string, cap int) *MemoryClient 
 	entry := logEntry.WithFields(log.Fields{
 		"package": "adapters.memory",
 	})
-	c := lfu.NewCache[string, string](lfu.WithCapacity(cap))
+	c := lru.NewCache[string, string](lru.WithCapacity(cap))
 	return &MemoryClient{
 		LogEntry: entry,
 		Prefix:   prefix,
@@ -27,7 +27,7 @@ func NewMemoryClient(logEntry *log.Entry, prefix string, cap int) *MemoryClient 
 }
 
 func (mc *MemoryClient) Cache(cacheable Cacheable[string]) error {
-	mc.LogEntry.Debugf("writing cacheable(%s) to memory(lfu cache)", cacheable.String())
+	mc.LogEntry.Debugf("writing cacheable(%s) to memory(lru cache)", cacheable.String())
 	pair, err := cacheable.GetPair()
 	if err != nil {
 		return err
@@ -44,7 +44,7 @@ func (mc *MemoryClient) GetCachable(cacheable Cacheable[string]) error {
 		return err
 	}
 	key = fmt.Sprintf("%s:%s", mc.Prefix, key)
-	mc.LogEntry.Debugf("reading cacheable(%s) from memory(lfu cache)", key)
+	mc.LogEntry.Debugf("reading cacheable(%s) from memory(lru cache)", key)
 	value, ok := mc.lfuCache.Get(key)
 	if !ok {
 		return ErrKeyNotExists
